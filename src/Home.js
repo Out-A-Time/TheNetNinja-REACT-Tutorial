a478d97c9fa2bd20dc49d9b5630322d10b2e6d05
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-07-02 16:31:12 +0100

    Lesson #19-Conditional Loading Message and handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -5,29 +5,8 @@ import BlogList from "./BlogList";
 const Home = function () {
   //useState - HOOK
   const [blogs, setBlogs] = useState(null);
-
-  // const [blogs, setBlogs] = useState([
-  //   {
-  //     title: "My new website in React",
-  //     body: "loremIpsum...",
-  //     author: "Mario",
-  //     id: 1,
-  //   },
-  //   {
-  //     title: "Hello from UpsideDown",
-  //     body: "loremIpsum...",
-  //     author: "Luigi",
-  //     id: 2,
-  //   },
-  //   {
-  //     title: "Learning React is fun",
-  //     body: "loremIpsum...",
-  //     author: "Wario",
-  //     id: 3,
-  //   },
-  // ]);
-
-  const [name, setName] = useState("Mario");
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // const handleDelete = function (id) {
   //   // const newBlogs = blogs.filter((blog) => blog.id !== id);  as arrow function-FILTER-when true stays, when false is filtered out from the newBlogs. Doesn't mutate original array.
@@ -42,14 +21,24 @@ const Home = function () {
 
   //useEffect - HOOK - run with EVERY render.
   //add empty 'dependency array' as second argument to avoid infinite loop -> USECASE: when we want to run function one time during first render
+
   useEffect(function () {
     fetch("http://localhost:8000/blogs")
-      .then((res) => {
+      .then(function (res) {
+        if (!res.ok) {
+          throw Error("Yo! We could not fetch the data for that resource");
+        }
         return res.json();
       })
-      .then((data) => {
-        console.log(data);
+      .then(function (data) {
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      //catching the network error
+      .catch(function (err) {
+        setIsLoading(false);
+        setError(err.message);
       });
     console.log("use effect ran");
   }, []);
@@ -57,6 +46,8 @@ const Home = function () {
   return (
     <div className="home">
       {/* condition templating output in React. Logical && evaluate left side first (if it false then won't do right side). At the beginning blogs is null, thus won't create an error. */}
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogsProp={blogs} titleProp={"All Posts"} />}
     </div>
   );
@@ -66,3 +57,4 @@ export default Home;
 
 //To run the JSON Server open second terminal and run
 //command "npx json-server --watch data/db.json --port8000"
+//or type 'npm run server' as command has been added into package.json
