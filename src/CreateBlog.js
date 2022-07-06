c9103adbc4f0ac55ae202f5b6a53f8ab13d15728
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-07-06 22:05:18 +0100

    Lesson #29-Making a POST request

--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -4,11 +4,22 @@ const CreateBlog = function () {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("wario");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = function (event) {
     event.preventDefault(); //prevents page being refreshed when submit
     const blog = { title, body, author };
-    console.log(blog);
+
+    setIsLoading(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST", //type of request
+      headers: { "Content-Type": "application/json" }, //Content type that is being sent
+      body: JSON.stringify(blog), //actual data that is being sent converted from an object into JSON. JSON-server will add ID automatically.
+    }).then(function () {
+      console.log("new blog added");
+      setIsLoading(false);
+    });
   };
 
   return (
@@ -42,7 +53,8 @@ const CreateBlog = function () {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isLoading && <button>Add Blog</button>}
+        {isLoading && <button>Adding Blog...</button>}
         <p>{author}</p>
       </form>
     </div>
